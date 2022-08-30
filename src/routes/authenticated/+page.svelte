ccb3cf127ef3ef466328376f3f55770c54a72846
Jonas Jongejan <jongejan@google.com>
2022-08-30 15:46:00 -0400

    fix: better typings

--- src/routes/authenticated/+page.svelte
+++ src/routes/authenticated/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { signOut, user } from '$lib/client.js';
-	import type { PageData } from './$types.js';
 </script>
 
 <h1>Example of page that requires authentication.</h1>
