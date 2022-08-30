ced78eae9ee3e19169167b5bbd23c6dec263fde6
Jonas Jongejan <jongejan@google.com>
2022-08-30 15:55:37 -0400

    fix: Dont export client in index

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,6 @@
 // Reexport your entry components here
 
-export { getClientId, getGapiClient, signIn, signOut, user } from './client.js';
+// export { getClientId, getGapiClient, signIn, signOut, user } from './client.js';
 export {
 	getAuthLocals,
 	getOAuth2Client,
